f28a39fd59883ead41f38f882b7f7f8e8b96d66e
Marty <martynolan17@gmail.com>
2022-08-09 10:13:32 -0400

    added html/api routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const path = require("path");
+const fs = require("fs");
+
+
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../db/db.json"));
+    
+  });
+
+  router.get("/notes/:id", (req, res) => {
+    let notesInFile = JSON.parse(fs.readFileSync("../../db/db.json", "utf8"))
+    res.json(notesInFile[Number(req.params.id)]);
+    
+  });
+
+
+  router.post("/notes", (req, res) => {
+    // set id based on what the next index of the array will be
+    
+    let notesInFile = JSON.parse(fs.readFileSync(
+        // path.join(__dirname, "../../db/db.json"), "utf8"));
+        path.join(__dirname, "../../db/db.json"), "utf8"));
+    req.body.id = notesInFile.length.toString();
+    
+    notesInFile.push(req.body);
+    fs.writeFileSync("../../db/db.json", JSON.stringify(notesInFile));
+      res.json(notesInFile);
+  });
+
+
+
+  module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require("express");
 
+const htmlRoutes = require("./routes/htmlRoutes/index");
+
+const apiRoutes = require("./routes/apiRoutes/index");
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Express.js middleware that instructs the server to make certain files readily available.
+app.use(express.static('public'));
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+app.use('/', htmlRoutes);
+
+app.use("/api", apiRoutes);
+
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
